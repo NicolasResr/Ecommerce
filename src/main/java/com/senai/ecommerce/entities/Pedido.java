34dd5d0dbb1cbce863d18c81954fdd6396360b3c
NicolasResr <npereirasantosdantas@gmail.com>
2025-02-04 17:07:21 -0300

    feat: adicionando entidades Categoria, Pagamento, Pedido, Usuario

---
+++ src/main/java/com/senai/ecommerce/entities/Pedido.java
@@ -0,0 +1,23 @@
+package com.senai.ecommerce.entities;
+
+import java.time.Instant;
+
+import com.senai.ecommerce.enun.StatusDoPedido;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_produto")
+public class Pedido {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private Instant momento;
+	private StatusDoPedido Status;
+
+}
---
+++ src/main/java/com/senai/ecommerce/enun/StatusDoPedido.java
@@ -0,0 +1,9 @@
+package com.senai.ecommerce.enun;
+
+public enum StatusDoPedido {
+	AGUARDANDO_PAGAMENTO, 
+	PAGO, 
+	ENVIADO,
+	ENTREGUE,
+	CANCELADO
+}
